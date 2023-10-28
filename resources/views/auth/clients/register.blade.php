9168b3f0f07c85be33069552e4edf00d36ee3928
Benjamin Sanchez <sanchez7888555@gmail.com>
2023-10-28 01:27:42 -0600

    feat: added more components.

--- resources/views/auth/clients/register.blade.php
+++ resources/views/auth/clients/register.blade.php
@@ -1,3 +1,5 @@
 <x-layouts.client-layout>
-
+    <div class="">
+        <x-input-text placeholder="Prueba de campo" id="name" name="name" value="{{ old('name') }}"/>
+    </div>
 </x-layouts.client-layout>
