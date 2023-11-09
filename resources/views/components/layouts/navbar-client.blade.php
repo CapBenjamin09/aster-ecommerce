acb49add8c91d844ba95de340c4e42ffe9ff8c47
Benjamin Sanchez <sanchez7888555@gmail.com>
2023-11-09 00:54:12 -0600

    feat: the navbar-client.blade.php was updated

--- resources/views/components/layouts/navbar-client.blade.php
+++ resources/views/components/layouts/navbar-client.blade.php
@@ -72,6 +72,26 @@
         <!-- Right elements -->
         <div class="relative flex items-center">
 
+
+            <!-- Cart Icon -->
+
+
+            <a
+                class="mr-4 text-white transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none [&.active]:text-black/90"
+                href="{{ route('cart.index') }}">
+                <span class="[&>svg]:w-5">
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="h-5 w-5">
+                    <path
+                        fill-rule="evenodd"
+                        d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                </span>
+            </a>
+
             @guest
                 <a
                     class="mr-4 text-white transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none [&.active]:text-black/90"
@@ -84,25 +104,8 @@
                     Crear cuenta
                 </a>
             @endguest
-            <!-- Cart Icon -->
 
-                @auth
-            <a
-                class="mr-4 text-white transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none [&.active]:text-black/90"
-                href="{{ route('cart.index') }}">
-        <span class="[&>svg]:w-5">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="h-5 w-5">
-            <path
-                fill-rule="evenodd"
-                d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-          </svg>
-        </span>
-            </a>
-
+            @auth
             <!-- Container with two dropdown menus -->
             <div
                 class="relative"
